d94ab1fbde6dbf57cf3615b2b802badc4fbdef2c
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-09-05 12:10:25 +0530

    Working on HeapSort algorithm

---
+++ Searching and Sorting/HeapSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int heapSize;
+    cout<<"Enter the size of the heap :";
+    cin>>heapSize;
+    vector<int> Maxheap(heapSize);
+    
+}

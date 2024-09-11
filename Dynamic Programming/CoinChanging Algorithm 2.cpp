99b077497b287807a9165554dca42a961fbf1d89
nithyasarathim <151494469+nithyasarathim@users.noreply.github.com>
2024-09-11 16:55:38 +0530

    Modified the code for coinchanging algorithm

--- Dynamic Programming/CoinChanging Algorithm 2.cpp
+++ Dynamic Programming/CoinChanging Algorithm 2.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main()
 {
+  int size;
+  cout<<"Enter the size total number of types of coins : ";
+  cin>>size;
+  int arr[size];
+  
   //The code could be written as soon as possible
 }
